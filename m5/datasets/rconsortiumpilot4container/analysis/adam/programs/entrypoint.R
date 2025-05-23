d1ef7773b16bce55fb5fc169c57056e954e3328c
Eric Nantz <theRcast@gmail.com>
2025-05-23 15:52:36 -0400

    fix: remove outdated build files

--- m5/datasets/rconsortiumpilot4container/analysis/adam/programs/entrypoint.R
+++
@@ -1,5 +0,0 @@
-options(shiny.port = 8787, shiny.host = "0.0.0.0")
-
-pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
-options( "golem.app.prod" = TRUE)
-pilot2wrappers::run_app() # add parameters here (if any)
